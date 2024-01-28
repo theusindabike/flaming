3033165925f12ebca8d196d02cbce3b26b1e50d6
Matheus Lopes <mlopes.matheus@gmail.com>
2024-01-28 19:43:31 -0300

    feat: initial commit

---
+++ lib/game/game.dart
@@ -0,0 +1,5 @@
+export 'components/components.dart';
+export 'cubit/cubit.dart';
+export 'entities/entities.dart';
+export 'flaming.dart';
+export 'view/view.dart';
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:flaming/app/app.dart';
+import 'package:flaming/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
